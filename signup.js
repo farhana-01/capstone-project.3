968d89132c7b6ba196e1481ca51824baedf6b862
Your Name <you@example.com>
2024-09-12 10:03:29 +0100

    i add my work

---
+++ signup.js
@@ -0,0 +1,14 @@
+
+
+
+
+            
+let singup = document.getElementById('singup').addEventListener('submit', function (event){
+    event.preventDefault();
+
+    let useEmail = document.getElementById('email') ;
+    let UesrPassword = document.getElementById('password') ;
+    console.log(email, password);
+    
+})
+ 
